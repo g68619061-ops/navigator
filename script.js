f645e220b7b99b1064cf755665f286e72738dc63
g68619061-ops <g68619061@gmail.com>
2026-05-20 18:25:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let city = document.querySelector("#city");
 let humidity = document.querySelector("#humidity");
 let feel = document.querySelector("#feel");
 let wind = document.querySelector("#wind");
+let deny = document.querySelector("#deny");
+let main = document.querySelector("#main");
+// let main = document.querySelector("#main");
 
 let la;
 let lo;
@@ -11,10 +14,17 @@ function a(data) {
     la = data.coords.latitude;
     lo = data.coords.longitude;
     console.log(`Latitude : ${la} Longitude : ${lo}`)
+    deny.style.display = "none";
+    main.style.display = "flex";
+
     jsd();
 }
 function b(error) {
     console.log("Error");
+    deny.style.display = "block";
+    deny.style.color = "red";
+    deny.innerHTML = "Trun On Location Service"
+    main.style.display = "none";
 }
 
 function c() {
@@ -26,16 +36,17 @@ c();
 
 async function jsd() {
     let myap = `https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=e405b5324908da8d44e445b9c12b6bbb&units=metric`;
-    try{
-    let s = await fetch(myap);
-    let gs = await s.json();
-    console.log(gs);
-    temp.innerHTML=`${Math.round(gs.main.temp)}°`;
-    city.innerHTML=`City : ${gs.name}`;
-    humidity.innerHTML=`Humidity : ${gs.main.humidity}%`;
-    feel.innerHTML=`Feels Like : ${Math.round(gs.main.feels_like)}°`;
-    wind.innerHTML=`Wind Speed ${gs.wind.speed}kmph`;
-} catch (rr) {
-    console.log("Error to fetch data");
-}
+    try {
+        let s = await fetch(myap);
+        let gs = await s.json();
+        console.log(gs);
+        temp.innerHTML = `${Math.round(gs.main.temp)}°`;
+        city.innerHTML = `City : ${gs.name}`;
+        humidity.innerHTML = `Humidity : ${gs.main.humidity}%`;
+        feel.innerHTML = `Feels Like : ${Math.round(gs.main.feels_like)}°`;
+        wind.innerHTML = `Wind Speed ${gs.wind.speed}kmph`;
+    } catch (rr) {
+        console.log("Error to fetch data");
+        alert("Problem to fetching data.")
+    }
 }
